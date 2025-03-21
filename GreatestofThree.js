7b0119586ef238bdc193ab5ebf95073cef48a30e
Ujwal Vilas Jakhamate <156206463+Ujwal1256@users.noreply.github.com>
2025-03-21 13:17:30 +0530

    Add files via upload

---
+++ GreatestofThree.js
@@ -0,0 +1,7 @@
+function getLargest(num1,num2,num3){
+  
+    (num1 > num2) ? (num1 > num3 ? console.log(num1)  : console.log(num3) )
+    :(num2 > num3 ? console.log(num2)  : console.log(num3) ) 
+    
+  }
+  getLargest(45,78,33)
